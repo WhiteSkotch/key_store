a4cd631b60536ef160d19b3c352243bab5f944b6
George Pogrebnoi <gosha1416@yandex.ru>
2023-06-15 23:54:09 +0300

    HUGE MEGA BUG FIX

--- test_project/settings.py
+++ test_project/settings.py
@@ -118,7 +118,7 @@ LOGIN_REDIRECT_URL = 'home'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",]
