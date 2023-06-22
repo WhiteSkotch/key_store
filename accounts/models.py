519036049b7ba85c81077f9ce5815fd9c6c575ab
White_Skotch <kalinina.anna22@mail.ru>
2023-06-22 11:07:32 +0300

    BD added

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,7 @@ class Game(models.Model):
     description = models.TextField()
     system_requirements = models.TextField()
     image = models.ImageField(upload_to='static/img')
+    discount = models.DecimalField(max_digits=3, decimal_places=0, default=5)
 
 
 class Key(models.Model):
@@ -24,5 +25,3 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     key = models.ForeignKey(Key, on_delete=models.CASCADE)
-    date_ordered = models.DateTimeField(auto_now_add=True)
-    is_payed = models.BooleanField(default=False)
